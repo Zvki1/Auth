f8cb9828d5abd3fb0bdeb4941584ab011ee21394
Zvki1 <rzaki@hotmail.fr>
2024-03-15 15:57:27 +0100

    fix(profile):dfixing white space in profile page

--- Front-End/CollabTelecom/src/pages/Profile.jsx
+++ Front-End/CollabTelecom/src/pages/Profile.jsx
@@ -6,20 +6,20 @@ const Profile = () => {
   return (
     <div className="bg-gray-300 h-screen">
       
-         <div className=" w-full h-176 bg-blue-800 pl-[369px] pr-[18px] pt-[21px] pb-[114px] flex justify-end items-center">
+         <div className=" w-full h-176 bg-blue-800  pr-[18px] pt-[21px] pb-[114px] flex justify-end items-center">
           <button className="bg-white rounded-3xl p-2.5 flex justify-center items-center">
            <SquarePen color="#0B4C8C"/>
           </button>
           
          </div>
-          <div className="flex flex-col items-center flex-col pt-[127px] pb-[117px] pl-[20px] pr-[20px]">
-            <div className="mb-[10px]">
+          <div className="flex flex-col items-center gap-3  pt-36  pl-[20px] pr-[20px]">
+            <div className=" w-full">
              <ProfileInfo icon={<User color="#0B4C8C"/>} text="Liste des amis"/>
             </div>
-            <div className="mb-[10px]">
+            <div className="10px] w-full">
              <ProfileInfo icon={<SlidersHorizontal color="#0B4C8C"/>} text="Paramètres"/>
             </div>
-            <div>
+            <div className="w-full">
              <ProfileInfo icon={<DoorOpen color="#D30000"/>} text="Se déconnecter"/>
             </div>
          </div>
