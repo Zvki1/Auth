20284717e7c37f9ef574267935870984270bfabf
lamis <121357000+Lamis-amari5@users.noreply.github.com>
2024-03-30 18:44:45 +0100

    profileinfo apdated

---
+++ Front-End/CollabTelecom/src/components/ListAmis/Header.jsx
@@ -0,0 +1,14 @@
+import { ChevronLeft } from "lucide-react";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <div className="w-full py-6 pl-3 flex items-center justify-between">
+      <Link to="/Profile">
+        <ChevronLeft size={32} />
+      </Link>
+      <h3 className="text-black font-Inter text-3xl font-semibold mx-auto ">Mes amis</h3>
+    </div>
+  );
+};
+
+export default Header;
--- Front-End/CollabTelecom/src/components/Profile/ProfileInfo.jsx
+++ Front-End/CollabTelecom/src/components/Profile/ProfileInfo.jsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
 
 
-const ProfileInfo =({ icon, text }) => {
+const ProfileInfo =({ icon, text, linkTo }) => {
     return (
         <>
+          <Link to={linkTo}>
            <button className=" w-full bg-gray-200 rounded-lg pl-[18px] py-[14px]    inline-flex items-center font-Inter font-[400]">
            <span className="icon mr-[6px]">{icon}</span>
            <span className=" font-inter font-[500] text-[23px]">{text}</span>
             </button> 
-            
+          </Link> 
         </>
     )
 }
