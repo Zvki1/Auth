262676948afe84cb7287b929e9c16ceca2639314
lamis <121357000+Lamis-amari5@users.noreply.github.com>
2024-03-31 12:40:16 +0100

    searchChat updated

--- Front-End/CollabTelecom/src/components/MessagesList/SearchChat.jsx
+++ Front-End/CollabTelecom/src/components/MessagesList/SearchChat.jsx
@@ -1,5 +1,12 @@
-
+import { useState } from "react";
 const SearchChat = () => {
+  const [searchFriend, setSearchFriend] = useState('');
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setSearchFriend(value); 
+    console.log(value); 
+};
+
   return (
    
 <form className="w-full px-5 pt-6">   
@@ -10,7 +17,7 @@ const SearchChat = () => {
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
-        <input type="search" id="default-search" className="block placeholder-[#167D8E] w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Rechercher"  />
+        <input value={searchFriend} onChange={handleChange} type="search" id="default-search" className="block placeholder-[#167D8E] w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Rechercher"  />
       
     </div>
 </form>
